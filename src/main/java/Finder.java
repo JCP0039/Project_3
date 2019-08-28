2f5a42c63c24d97076ad7be5c3edf41570e4d0df
James <JCP0039@auburn.edu>
2019-08-28 18:38:10 -0500

    attempt

---
+++ src/main/java/Finder.java
@@ -0,0 +1,47 @@
+public class Finder {
+
+    public java.lang.Integer findMax(int[] intArray) {
+        if(intArray == null) {
+            return null;
+        }
+        if(intArray.length == 0) {
+            return null;
+        }
+        int max = intArray[0];
+        int i = 1;
+
+        while (i < intArray.length) {
+
+            if(intArray[i] > max){
+              max = intArray[i];
+            }
+
+            i++;
+        }
+        return max;
+    }
+
+    public java.lang.Integer findMin(int[] intArray) {
+        if(intArray == null) {
+            return null;
+        }
+        if(intArray.length == 0) {
+            return null;
+        }
+        int min = intArray[0];
+        int i = 1;
+
+        while (i < intArray.length) {
+
+            if(intArray[i] < min){
+                min = intArray[i];
+            }
+
+            i++;
+        }
+        return min;
+    }
+
+
+
+}
